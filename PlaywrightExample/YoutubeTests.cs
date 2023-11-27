49aa951cce5bcc10107e508a616067cec1083b94
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-11-27 15:25:40 +0700

    feat: Add codegen command

--- PlaywrightExample/YoutubeTests.cs
+++ PlaywrightExample/YoutubeTests.cs
@@ -8,10 +8,12 @@ public class YoutubeTests : PageTest
     [Test]
     public async Task MyTestMethod()
     {
+        // pwsh bin/Debug/net8.0/playwright.ps1 codegen playwright.dev
         await Page.GotoAsync("https://www.youtube.com/");
-        await Page.GetByPlaceholder("Search").ClickAsync();
-        await Page.GetByPlaceholder("Search").FillAsync("Stay");
-        await Page.GetByPlaceholder("Search").PressAsync("Enter");
+        var searchInput = Page.GetByPlaceholder("Search");
+        await searchInput.ClickAsync();
+        await searchInput.FillAsync("Stay");
+        await searchInput.PressAsync("Enter");
         await Page.WaitForURLAsync(Page.Url);
         await Page.Locator("ytd-video-preview").First.ClickAsync();
         await Page.WaitForURLAsync(Page.Url);
